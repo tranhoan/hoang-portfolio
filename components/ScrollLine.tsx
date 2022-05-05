4bc08df5001280a744d28ee3801333ae18806b00
Hoang Tran <tranhoan@fel.cvut.cz>
2022-05-05 16:57:29 +0200

    :lipstick: add page animation

--- components/ScrollLine.tsx
+++ components/ScrollLine.tsx
@@ -16,16 +16,24 @@ const ScrollLine: React.FC = () => {
         <S.ScrollLine />
       </S.ScrollContainer>
       <S.SectionPagination>
-        <S.Numerator>
-          <S.Page>01</S.Page>
-          <S.Page>02</S.Page>
-          <S.Page>03</S.Page>
-        </S.Numerator>
-        / 03
+        <Numerator page={sections[nextActiveSection].sectionNumber + 1}>
+          <S.FirstPage>01</S.FirstPage>
+          <S.SecondPage>02</S.SecondPage>
+          <S.ThirdPage>03</S.ThirdPage>
+        </Numerator>
+        <S.Slash>/</S.Slash>
+        <S.TotalPages>03</S.TotalPages>
       </S.SectionPagination>
     </S.Footer>
   );
 };
+
+const paginationCss = css`
+  display: inline-block;
+  top: 0;
+  left: 0;
+  transition: transform 700ms cubic-bezier(0.645, 0.045, 0.355, 1);
+`;
 
 const S = {
   TextWrapper: styled.div`
@@ -63,10 +71,69 @@ const S = {
     padding-left: 3.2rem;
     min-width: 6.4rem;
     transition: color 1s cubic-bezier(0.645, 0.045, 0.355, 1);
+    overflow: hidden;
   `,
 
-  Numerator: styled.span``,
-
-  Page: styled.span``,
+  FirstPage: styled.span`
+    position: absolute;
+    ${paginationCss}
+  `,
+  SecondPage: styled.span`
+    position: relative;
+    ${paginationCss}
+  `,
+  ThirdPage: styled.span`
+    position: absolute;
+    ${paginationCss}
+  `,
+  Slash: styled.span`
+    padding: 0 1.2rem;
+  `,
+  TotalPages: styled.span``,
 };
+
+const Numerator = styled.span<{ page: number }>`
+  position: relative;
+  /* margin-right: 1.6rem; */
+  display: inline-block;
+  ${(props) =>
+    props.page === 1 &&
+    css`
+      ${S.FirstPage} {
+        transform: translate3d(0px, 0%, 0px);
+      }
+      ${S.SecondPage} {
+        transform: translate3d(0px, 100%, 0px);
+      }
+      ${S.ThirdPage} {
+        transform: translate3d(0px, 200%, 0px);
+      }
+    `}
+  ${(props) =>
+    props.page === 2 &&
+    css`
+      ${S.FirstPage} {
+        transform: translate3d(0px, -100%, 0px);
+      }
+      ${S.SecondPage} {
+        transform: translate3d(0px, 0%, 0px);
+      }
+      ${S.ThirdPage} {
+        transform: translate3d(0px, 100%, 0px);
+      }
+    `}
+  ${(props) =>
+    props.page === 3 &&
+    css`
+      ${S.FirstPage} {
+        transform: translate3d(0px, -200%, 0px);
+      }
+      ${S.SecondPage} {
+        transform: translate3d(0px, -100%, 0px);
+      }
+      ${S.ThirdPage} {
+        transform: translate3d(0px, 0%, 0px);
+      }
+    `}
+`;
 export default ScrollLine;
